c221d66d857b04db3ec63a61f2ed7c1d6f9ee599
ctruo <ctruongg67@gmail.com>
2022-10-18 01:06:39 -0700

    Added buttons/icons for settings

--- src/components/Navigation/Settings.js
+++ src/components/Navigation/Settings.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./Settings.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faCloud, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun as faSunReg } from "@fortawesome/free-regular-svg-icons";
 
 function Settings() {
   return (
     <ul className="settings">
       <li>
-        <h3>Setting 1</h3>
+        <h3>Theme</h3>
+        <div className="theme-button-list">
+          <button className="theme-button">
+            <FontAwesomeIcon icon={faSunReg} className="fa-xl" />
+          </button>
+          <button className="theme-button">
+            <FontAwesomeIcon icon={faSun} className="fa-xl" />
+          </button>
+          <button className="theme-button">
+            <FontAwesomeIcon icon={faMoon} className="fa-xl" />
+          </button>
+          <button className="theme-button">
+            <FontAwesomeIcon icon={faCloud} className="fa-xl" />
+          </button>
+        </div>
       </li>
       <li>
-        <h3>Setting 1</h3>
-      </li>
-      <li>
-        <h3>Setting 1</h3>
-      </li>
-      <li>
-        <h3>Setting 1</h3>
+        <h3>Temperature Scale</h3>
+        <div className="temp-scale-list">
+          <button className="temp-scale-button">{`\u00B0F`}</button>
+          <b class="temp-scale-slash"> / </b>
+          <button className="temp-scale-button">{`\u00B0C`}</button>
+        </div>
       </li>
     </ul>
   );
